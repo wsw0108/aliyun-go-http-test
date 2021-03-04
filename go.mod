ccbca584b82a8f99195a69ec23a78be5d110ad9d
wsw0108 <wsw0108@gmail.com>
2021-03-04 21:10:28 +0800

    gin rewrite not working, use echo instead

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module example
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.2.0
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,48 @@
 package main
 
 import (
+	"net/http"
 	"strings"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func StripFcBasePath(c *gin.Context) {
-	req := c.Request
-	basePath := req.Header.Get("x-fc-base-path")
-	if basePath != "" {
-		replacer := strings.NewReplacer(basePath, "")
-		uri := replacer.Replace(req.URL.Path)
-		req.RequestURI = uri
-		req.URL.Path = uri
+func StripFcBasePath() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			req := c.Request()
+			url := req.URL
+			basePath := req.Header.Get("x-fc-base-path")
+			if basePath != "" {
+				replacer := strings.NewReplacer(basePath, "")
+				uri := replacer.Replace(req.URL.Path)
+				req.RequestURI = uri
+				url.Path = uri
+			}
+			return next(c)
+		}
 	}
-	c.Next()
 }
 
 func main() {
-	r := gin.Default()
-	r.Use(StripFcBasePath)
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run("0.0.0.0:9000")
+	// Echo instance
+	e := echo.New()
+
+	e.Pre(StripFcBasePath())
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/ping", ping)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":9000"))
+}
+
+// Handler
+func ping(c echo.Context) error {
+	return c.String(http.StatusOK, "pong!")
 }
